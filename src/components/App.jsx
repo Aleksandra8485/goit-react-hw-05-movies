18415100691dd4a975ee3213034e2f6c27d48191
Aleksandra <a.walczak1984@gmail.com>
2023-07-07 14:45:11 +0200

    changes in api.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -26,8 +26,8 @@ const App = () => {
         <Route path="/" element={<HomePage />} />
         <Route path="/movies" element={<Movies />} />
         <Route path="/movies/:movieId" element={<MovieDetails />}>
-          <Route path="/cast" element={<Cast />} />
-          <Route path="/reviews" element={<Reviews />} />
+          <Route path="cast" element={<Cast />} />
+          <Route path="reviews" element={<Reviews />} />
         </Route>
       </Routes>
     </div>
--- src/utils/api.jsx
+++ src/utils/api.jsx
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
+//Ustawienie baseURL dla wszystkich zapytań Axios
+//Ustawienie api_key jako domyślnego parametru
 const BASE_URL = 'https://api.themoviedb.org/3';
 const API_KEY = '5aa46e061141b6aa48191a62115cc996';
 
@@ -8,36 +10,16 @@ axios.defaults.params = {
   api_key: API_KEY,
 };
 
-// // Ustawienie baseURL dla wszystkich zapytań Axios
-// axios.defaults.baseURL = 'https://api.themoviedb.org/3';
-
-// // Ustawienie api_key jako domyślnego parametru
-// axios.defaults.params = {
-//   api_key: '5aa46e061141b6aa48191a62115cc996',
-// };
-
 //pobranie aktualnych filmów
 
 const getTrendingMovies = async () => {
   try {
-    const response = await axios.get(
-      `${BASE_URL}/trending/movie/day?api_key=${API_KEY}`
-    );
+    const response = await axios.get(`/trending/movie/day`);
     return response.data.results;
   } catch (error) {
     console.error('Błąd podczas pobierania najpopularniejszych filmów:', error);
     throw error;
   }
 };
-
-// const fetchTrendingMovies = async () => {
-//   try {
-//     const response = await axios.get('/trending/movie/day');
-//     return response.data.results;
-//   } catch (error) {
-//     console.log(error);
-//     return [];
-//   }
-// };
 
 export { getTrendingMovies };
